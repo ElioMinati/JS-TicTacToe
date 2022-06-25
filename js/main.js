a6a5b7fe29f442f0d260e280f7bd11b3e0b938e7
Eliott JOUAN <eliott.jouan@epitech.eu>
2022-06-25 13:07:44 +0200

    [ADD] css, buttons don't move when filled anymore, can reset grid, and also displays winner

--- js/main.js
+++ js/main.js
@@ -22,8 +22,27 @@ const addInGrid = nb => {
     winInterval = setInterval(verifyWinCondition, 100);
 };
 
+const resetGrid = () => {
+    for (let i = 1; i < 10; i++) {
+        document.getElementById(i).innerHTML = '';
+        document.getElementById(i).disabled = false;
+    }
+    document.getElementById('starter').value === '2' ? turn = 1 : turn = 0;
+    document.getElementById('winner').innerHTML = '';
+}
+
 const displayWin = player => {
-    alert(`player ${player} won !`);
+    for (let i = 1; i < 10; i++) {
+        if (!document.getElementById(i).innerHTML) {
+            document.getElementById(i).innerHTML = '-';
+        }
+        document.getElementById(i).disabled = true;
+    }
+    if (player === -1) {
+        document.getElementById('winner').innerHTML = `it's a draw !`;
+        return;
+    }
+    document.getElementById('winner').innerHTML = `Player ${player} has won!`;
 }
 
 const fillTacArray = () => {
@@ -52,20 +71,28 @@ const verifyWinCondition = () => {
 
     for (let i = 0; i < 3; i++) {
         if (tmp[i][0] === tmp[i][1] && tmp[i][1] === tmp[i][2] && tmp[i][0] !== 0) {
-            displayWin(tmp[i][0]);
+            tmp[i][0] === 'X' ? displayWin(1) : displayWin(2);
             return;
         }
         if (tmp[0][i] === tmp[1][i] && tmp[1][i] === tmp[2][i] && tmp[0][i] !== 0) {
-            displayWin(tmp[0][i]);
+            tmp[0][i] === 'X' ? displayWin(1) : displayWin(2);
             return;
         }
     }
     if (tmp[0][0] === tmp[1][1] && tmp[2][2] === tmp[1][1] && tmp[0][0] !== 0) {
-        displayWin(tmp[0][0]);
+        tmp[0][0] === 'X' ? displayWin(1) : displayWin(2);
         return;
     }
     if (tmp[2][0] === tmp[1][1] && tmp[0][2] === tmp[1][1] && tmp[2][0] !== 0) {
-        displayWin(tmp[2][0]);
+        tmp[2][0] === 'X' ? displayWin(1) : displayWin(2);
         return;
     }
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (tmp[i][j] === 0) {
+                return;
+            }
+        }
+    }
+    displayWin(-1);
 }
